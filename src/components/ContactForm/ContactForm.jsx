58ad46b7651deb81096695883f1cda1daf8ecee7
holus-bolus <ustinovoleksij@gmail.com>
2024-01-02 08:38:43 +0200

    Added the contact form

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,5 +1,69 @@
+import Footer from '../Footer/Footer.jsx';
+import './ContactForm.css';
+
 const ContactForm = () => {
-  return <div>Contact form</div>;
+  return (
+    <div className="container my-4">
+      <h2 className="mb-3 heading">Our Contacts</h2>
+      <p className="lead">How you can contact us</p>
+
+      <div className="row">
+        <div className="col-md-6">
+          <div className="custom-card-bg">
+            <div className="card p-4 my-3">
+              <h3 className="card-title">Ornamentico Group Company</h3>
+              <address className="card-text">
+                Koprivnik v Bohinju 5,
+                <br />
+                Bohinjska Bistrica,
+                <br />
+                4264, Slovenia
+                <br />
+                <a href="mailto:hello@ornamentico.com" className="card-link">
+                  hello🐶ornamentico.com
+                </a>
+              </address>
+            </div>
+          </div>
+
+          <a href="https://goo.gl/maps/8DtJQ6ZXCas" className="d-block mb-3">
+            How to reach us
+          </a>
+
+          <div className="embed-responsive embed-responsive-16by9 mb-4">
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2756.255226431165!2d13.981450476742989!3d46.304768476963154!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x477a929aa9d34815%3A0xc92767cbe8983fe2!2sKoprivnik%20v%20Bohinju%205%2C%204264%20Bohinjska%20Bistrica%2C%20Slovenia!5e0!3m2!1sen!2sua!4v1704176323152!5m2!1sen!2sua"
+              className="embed-responsive-item"
+              allowFullScreen=""
+              loading="lazy"
+              referrerPolicy="no-referrer-when-downgrade"
+            ></iframe>
+          </div>
+
+          <p>
+            How to get there: it's easy to get to us. After turning off the road
+            at the Bled sign, you need to drive through the town of Bled and
+            continue on road 209 towards Lake Bohinj. In the village of Bitnje,
+            turn right at the Pokljuka / Srednja Vas / Jereka sign. Further 2 km
+            along the road 633 (light serpentine) to the village of Jereka, in
+            which turn right again at the sign Poklujka / Koprivnik / Jereka.
+          </p>
+          <p>
+            Further along the road 905 you need to drive 6 km of real mountain
+            serpentine, and you come to us! Our apartments hotel Villa Manja is
+            the first house at the entrance to the village of Koprivnik.
+          </p>
+          <p>
+            If you use Google Maps or MapsMe, just type Villa Manja Slovenia in
+            the search bar and the app will easily guide you to our cozy
+            apartments hotel.
+          </p>
+        </div>
+      </div>
+
+      <Footer />
+    </div>
+  );
 };
 
 export default ContactForm;
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,112 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <ul className="d-flex justify-content-between align-items-center list-unstyled">
+          <li>
+            <a
+              href={'https://www.facebook.com/Ornamentico/'}
+              target={'_blank'}
+              className={'custom-link'}
+            >
+              <img
+                src="/facebook-svgrepo-com.svg"
+                alt="A Facebook icon"
+                className={'footer-icon'}
+              />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.instagram.com/ornamentico_slovenia/"
+              target={'_blank'}
+              className={'custom-link'}
+            >
+              <img
+                src="/instagram-1-svgrepo-com.svg"
+                alt="An Instagram icon"
+                className={'footer-icon'}
+              />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.youtube.com/@villamanja_slovenia"
+              target={'_blank'}
+              className={'custom-link'}
+            >
+              <img
+                src="/youtube-color-svgrepo-com.svg"
+                alt="A YouTube icon"
+                className={'footer-icon'}
+              />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.pinterest.com/Ornamenticoshop/"
+              target={'_blank'}
+              className={'custom-link'}
+            >
+              <img
+                src="/pinterest-1-svgrepo-com.svg"
+                alt="A Pinterest icon"
+                className={'footer-icon'}
+              />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.linkedin.com/company/ornamenticogroup/"
+              target={'_blank'}
+              className={'custom-link'}
+            >
+              <img
+                src="/linkedin-svgrepo-com.svg"
+                alt="A LinkedIn icon"
+                className={'footer-icon'}
+              />
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div className="container">
+        <ul className="list-unstyled">
+          <li className="row mb-2">
+            <div className="col-12 col-md-auto">
+              <span className="contacts-item">
+                Contacts: Ornamentico Group |
+              </span>
+            </div>
+            <div className="col-12 col-md-auto">
+              <span className="contacts-item">www.ornamentico.com | </span>
+            </div>
+            <div className="col-12 col-md-auto">
+              <span className="contacts-item">
+                Koprivnik v Bohinju 5, 4264, Bohinjska Bistrica, Slovenia |
+              </span>
+            </div>
+            <div className="col-12 col-md-auto">
+              <span className="contacts-item">
+                <a
+                  className={'custom-link'}
+                  href={'mailto:hello@ornamentico.com'}
+                >
+                  hello🐶ornamentico.com
+                </a>
+              </span>
+            </div>
+          </li>
+        </ul>
+        <div className="mt-3">
+          <p>&copy; 2016 Ornamentico Group</p>
+          <p>Koprivnik v Bohinju 5, Bohinjska Bistrica, 4264, Slovenia</p>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
