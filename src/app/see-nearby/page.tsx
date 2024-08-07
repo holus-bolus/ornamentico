08ccd6516a4e5a8edb6d28794fbf0f7a165770d7
holus-bolus <ustinovoleksij@gmail.com>
2024-08-07 18:45:58 +0300

    Chamged the see nearby page

---
+++ src/app/see-nearby/page.tsx
@@ -0,0 +1,9 @@
+const SeeNearby = () => {
+    return (
+        <section>
+            <h2>What to see near our Apart Hotel Villa Manja</h2>
+        </section>
+    );
+};
+
+export default SeeNearby;
